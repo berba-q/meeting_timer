049137c2e3088a6fc60f283ecff8b2182b937703
Griffiths Laryea <griffiths.oblilaryea@fao.org>
2025-04-23 21:37:47 +0200

    unit testing

---
+++ src/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Unit tests for JW Meeting Timer application.
+"""
+import sys
+import os
+from pathlib import Path
+
+# Add the parent directory to the path so we can import the application code
+sys.path.insert(0, str(Path(__file__).parent.parent))
